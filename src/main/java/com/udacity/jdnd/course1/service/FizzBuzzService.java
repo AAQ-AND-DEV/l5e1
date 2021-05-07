25b061e4fe8aae311ce1966fc128fdeae9c8d896
aaq-and-dev <aaqjavadevmentor@gmail.com>
2021-05-06 17:02:11 -0700

    * implemented fizzBuzz. simple modulus operations, with if elseif, etc

--- src/main/java/com/udacity/jdnd/course1/service/FizzBuzzService.java
+++ src/main/java/com/udacity/jdnd/course1/service/FizzBuzzService.java
@@ -9,8 +9,26 @@ public class FizzBuzzService {
      *
      * @Throws IllegalArgumentException for values < 1
      */
-    public String fizzBuzz(int number) {
-        return "";
+    public String fizzBuzz(int number) throws IllegalArgumentException {
+        StringBuilder sb = new StringBuilder();
+        if (number <= 0) {
+            throw new IllegalArgumentException();
+        }
+        boolean divBy3 = divBy(number, 3);
+        boolean divBy5 = divBy(number, 5);
+        if (divBy3 && divBy5) {
+            sb.append("FizzBuzz");
+        } else if (divBy3) {
+            sb.append("Fizz");
+        } else if (divBy5) {
+            sb.append("Buzz");
+        } else {
+            return String.valueOf(number);
+        }
+        return sb.toString();
     }
 
+    private boolean divBy(int num, int div) {
+        return num % div == 0;
+    }
 }
